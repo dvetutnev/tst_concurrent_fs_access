25990d5b355652ee95c04affa3c648211a32963f
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-05-22 13:05:18 +0300

    Test filepath wchar, use conversion from utf8

--- test_fs_errors.cpp
+++ test_fs_errors.cpp
@@ -2,7 +2,7 @@
 
 #include <boost/filesystem/fstream.hpp>
 #include <boost/filesystem/path.hpp>
-#include <boost/locale/generator.hpp>
+#include <boost/locale.hpp>
 
 #include <string>
 #include <iostream>
@@ -28,7 +28,9 @@ TEST(FS, cyrillic_filepath_wchar) {
     std::locale loc{boost::locale::generator().generate("")};
     boost::filesystem::path::imbue(loc);
 
-    boost::filesystem::path filepath = L"Кириллица.txt";
+    const std::string filepath_utf8 = "Кириллица.txt";
+    const std::wstring filepath_utf16 = boost::locale::conv::utf_to_utf<wchar_t>(filepath_utf8);
+    boost::filesystem::path filepath = filepath_utf16;
     boost::filesystem::ifstream f{filepath};
     ASSERT_TRUE(f.is_open());
 }
