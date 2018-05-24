fff7dc94609f5b841c1d68a8088aa12f81a6189f
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-05-24 18:01:35 +0300

    readDirectory, by mask, first release

--- fs/directory.cpp
+++ fs/directory.cpp
@@ -15,21 +15,50 @@ namespace oda {
 namespace fs {
 
 
+namespace internal {
+
+
+class Glob
+{
+public:
+
+    Glob(const Path&);
+    const Path& dirPath() const;
+    bool match(const Path&) const;
+
+private:
+
+    bool match(std::string::const_iterator, std::string::const_iterator, std::string::const_iterator, std::string::const_iterator) const;
+
+    Path _patternPath;
+    Path _dirPath;
+};
+
+
+} // namspace internal
+
+
 Directory readDirectory(const Path& path) {
 
     Directory result;
 
     try {
 
-        boost::filesystem::directory_iterator it{path};
+        internal::Glob glob{path};
+        boost::filesystem::directory_iterator it{glob.dirPath()};
         const boost::filesystem::directory_iterator end;
+
         for (; it != end; ++it) {
 
             const auto& entryPath = it->path();
-            const auto& fileStatus = it->status();
-            const bool isDirectory = fileStatus.type() == boost::filesystem::directory_file;
 
-            result.push_back(DirectoryEntry{entryPath, isDirectory});
+            if (glob.match(entryPath)) {
+
+                const auto& fileStatus = it->status();
+                const bool isDirectory = fileStatus.type() == boost::filesystem::directory_file;
+
+                result.push_back(DirectoryEntry{entryPath, isDirectory});
+            }
         }
     } catch (const boost::filesystem::filesystem_error& e) {
 
@@ -40,6 +69,91 @@ Directory readDirectory(const Path& path) {
 
     return result;
 }
+
+
+namespace internal {
+
+
+Glob::Glob(const Path& path) {
+
+    const std::string& tail = path.filename().string();
+    if (tail.find_first_of("*?") != std::string::npos) {
+
+        _patternPath = path;
+        _dirPath = path.parent_path();
+
+    } else {
+
+        _dirPath = path;
+    }
+}
+
+const Path& Glob::dirPath() const {
+
+    return _dirPath;
+}
+
+bool Glob::match(const Path& entryPath) const {
+
+    if (_patternPath.empty()) {
+
+        return true;
+    }
+
+    const std::string& pattern = _patternPath.string();
+    const std::string& entry = entryPath.string();
+
+    return match(std::cbegin(pattern), std::cend(pattern), std::cbegin(entry), std::cend(entry));
+}
+
+bool Glob::match(std::string::const_iterator pIt, std::string::const_iterator pEndIt, std::string::const_iterator sIt, std::string::const_iterator sEndIt) const {
+
+    while (pIt != pEndIt) {
+
+        if (sIt == sEndIt) {
+
+            while (pIt != pEndIt && *pIt == '*') {
+                ++pIt;
+            }
+            break;
+        }
+
+        switch (*pIt) {
+
+        case '?':
+
+            ++pIt;
+            ++sIt;
+            break;
+
+        case '*':
+
+            if (++pIt != pEndIt) {
+
+                while (sIt != sEndIt && !match(pIt, pEndIt, sIt, sEndIt)) {
+                    ++sIt;
+                }
+                return sIt != sEndIt;
+            }
+
+            return true;
+
+        default:
+
+            if (*pIt != *sIt) {
+
+                return false;
+            }
+
+            ++pIt;
+            ++sIt;
+        }
+    }
+
+    return pIt == pEndIt && sIt == sEndIt;
+}
+
+} // namespace internal
 
 
 } // namespace fs
