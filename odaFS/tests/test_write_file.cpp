f0ef0be019ff426d7d0a04b386c6eb82a3e8d08c
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-06-08 20:41:53 +0300

    fileWrite scel

---
+++ odaFS/tests/test_write_file.cpp
@@ -0,0 +1,21 @@
+#include <fs/file.h>
+#include <fs/directory.h>
+#include "dataset.h"
+
+#include <gtest/gtest.h>
+
+
+TEST(Fake, Fake) {
+
+    SUCCEED();
+}
+
+
+TEST(writeFile, truncate) {
+
+}
+
+
+TEST(writeFile, append) {
+
+}
